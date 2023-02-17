d256234837ecb8d3f29be76207516ecf57ad8276
ITX-AL3X <118067650+ITX-AL3X@users.noreply.github.com>
2023-02-17 22:18:59 +0500

    Delete Alex.py

--- Alex.py
+++
@@ -1,9 +0,0 @@
-from platform import uname
-from os import system as xd
-xd('git pull')
-bit=uname().machine.lower()
-if 'aarch' in bit:
-    import Alex
-    Alex.menu()
-else:
-    exit(' 32bit Device not Supported ')
